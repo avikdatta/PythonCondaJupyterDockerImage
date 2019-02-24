805352bd0e331c0725abd53536c691389216a2c4
avik <reach4avik@yahoo.com>
2019-02-24 20:41:57 +0000

    Create entrypoint.sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+case "$1" in
+  webserver)
+  sleep 10
+  exec jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.iopub_data_rate_limit=100000000
+  ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec "$@"
+    ;;
+esac
